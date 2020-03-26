0edad1add7675bc0d63fe5c5acd6e22edccbeb1e
SGO-Nova <51762752+SGO-Nova@users.noreply.github.com>
2020-03-26 18:23:47 -0500

    Created options for AI and shuffled characters

--- BANG-The_Game/src/bang/BANG.java
+++ BANG-The_Game/src/bang/BANG.java
@@ -5,6 +5,10 @@
  */
 package bang;
 
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author ryanb
@@ -15,25 +19,50 @@ public class BANG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Create all the character cards with the names and HP. Attributes to come later!
-        Character_Cards character1 = new Character_Cards("Bart Cassidy", 8);
-        Character_Cards character2 = new Character_Cards("Black Jack", 8);
-        Character_Cards character3 = new Character_Cards("Calamity Janet", 8);
-        Character_Cards character4 = new Character_Cards("El Gringo", 7);
-        Character_Cards character5 = new Character_Cards("Jesse Jones", 9);
-        Character_Cards character6 = new Character_Cards("Joursonnais", 7);
-        Character_Cards character7 = new Character_Cards("Kit Carlson", 7);
-        Character_Cards character8 = new Character_Cards("Lucky Duke", 8);
-        Character_Cards character9 = new Character_Cards("Paul Regret", 9);
-        Character_Cards character10 = new Character_Cards("Pedro Ramirez", 8);
-        Character_Cards character11 = new Character_Cards("Rose Doolan", 9);
-        Character_Cards character12 = new Character_Cards("Sid Ketchum", 8);
-        Character_Cards character13 = new Character_Cards("Slab The Killer", 8);
-        Character_Cards character14 = new Character_Cards("Suzy Lafayette", 8);
-        Character_Cards character15 = new Character_Cards("Vulture Sam", 9);
-        Character_Cards character16 = new Character_Cards("Willy The Kid", 8);
+        //Create character cards, dice, and role cards
+        ArrayList<Character_Cards> char_cards = new ArrayList<Character_Cards>();
+        char_cards.add(new Character_Cards("Bart Cassidy", 8));
+        char_cards.add(new Character_Cards("Black Jack", 8));
+        char_cards.add(new Character_Cards("Calamity Janet", 8));
+        char_cards.add(new Character_Cards("El Gringo", 7));
+        char_cards.add(new Character_Cards("Jesse Jones", 9));
+        char_cards.add(new Character_Cards("Joursonnais", 7));
+        char_cards.add(new Character_Cards("Kit Carlson", 7));
+        char_cards.add(new Character_Cards("Lucky Duke", 8));
+        char_cards.add(new Character_Cards("Paul Regret", 9));
+        char_cards.add(new Character_Cards("Pedro Ramirez", 8));
+        char_cards.add(new Character_Cards("Rose Doolan", 9));
+        char_cards.add(new Character_Cards("Sid Ketchum", 8));
+        char_cards.add(new Character_Cards("Slab The Killer", 8));
+        char_cards.add(new Character_Cards("Suzy Lafayette", 8));
+        char_cards.add(new Character_Cards("Vulture Sam", 9));
+        char_cards.add(new Character_Cards("Willy The Kid", 8));
+        Collections.shuffle(char_cards);
         
-        //Test out character hp damage and heal
+        Dice dice1 = new Dice(0);
+        Dice dice2 = new Dice(0);
+        Dice dice3 = new Dice(0);
+        Dice dice4 = new Dice(0);
+        Dice dice5 = new Dice(0);
+        
+        ArrayList<Role_Cards> role_cards = new ArrayList<Role_Cards>();
+
+        Scanner scan;
+        scan = new Scanner(System.in);
+        int players;
+        Random rand = new Random();
+        Player human = new Player("NULL",0,"NULL",false);
+        Player ai1 = new Player("NLUL", 0, "NULL", true);
+        Player ai2 = new Player("NLUL", 0, "NULL", true);
+        Player ai3 = new Player("NLUL", 0, "NULL", true);
+        Player ai4 = new Player("NLUL", 0, "NULL", true);
+        Player ai5 = new Player("NLUL", 0, "NULL", true);
+        Player ai6 = new Player("NLUL", 0, "NULL", true);
+        Player ai7 = new Player("NLUL", 0, "NULL", true);
+
+        
+        
+        /*Test out character hp damage and heal
         System.out.println("Bart has " + character1.hp);
         System.out.println("Black Jack has " + character2.hp);
         System.out.println("Black Jack shot him");
@@ -43,29 +72,120 @@ public class BANG {
         System.out.println("Black Jack drank beer");
         character2.heal(1);
         System.out.println("Black Jack now has " + character2.hp);
-        
-        Dice dice1 = new Dice(0);
-        Dice dice2 = new Dice(0);
-        Dice dice3 = new Dice(0);
-        Dice dice4 = new Dice(0);
-        Dice dice5 = new Dice(0);
+        */
+        /* Testing dice rolls
         System.out.println(dice1.roll()+ ", " +dice2.roll()+ ", " +dice3.roll()+ ", " +dice4.roll()+ ", " +dice5.roll());
         System.out.println("Rerolling dice 4 and 5!");
         System.out.println(dice1.keep() + ", " + dice2.keep() + ", " + dice3.keep() + ", " + dice4.roll() + ", " + dice5.roll());
-        
-        //Create role cards
-        Role_Cards card0 = new Role_Cards("Sheriff");
-        Role_Cards card1 = new Role_Cards("Renegade");
-        Role_Cards card2 = new Role_Cards("Outlaw");
-        Role_Cards card3 = new Role_Cards("Outlaw");
-        Role_Cards card4 = new Role_Cards("Deputy");
-        Role_Cards card5 = new Role_Cards("Outlaw");
-        Role_Cards card6 = new Role_Cards("Deputy");
-        Role_Cards card7 = new Role_Cards("Renegade");
-        
-        //Test out character object;
+        */
+        /*Test out character object;
         Player human = new Player(character5.name, character5.hp, card2.role, false);
         System.out.println("This player's name is: " + human.name + ", has " + human.health +" hp, and is a " + human.role);
+        */
+        
+        do{
+            System.out.println("How many people do you want to play against? (2-7 AIs)");
+            System.out.print("> ");
+            players = scan.nextInt();
+
+            switch(players){
+                case 2: 
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Deputy"));
+                    role_cards.add(new Role_Cards("Renegade"));
+                    Collections.shuffle(role_cards);
+                    human = new Player(char_cards.get(0).name, char_cards.get(0).hp, role_cards.get(0).role, false);
+                    ai1 = new Player(char_cards.get(1).name, char_cards.get(1).hp, role_cards.get(1).role, true);
+                    ai2 = new Player(char_cards.get(2).name, char_cards.get(2).hp, role_cards.get(2).role, true);
+                    break;
+                case 3:
+                    role_cards.add(new Role_Cards("Sheriff"));
+                    role_cards.add(new Role_Cards("Renegade"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    Collections.shuffle(role_cards);
+                    human = new Player(char_cards.get(0).name, char_cards.get(0).hp, role_cards.get(0).role, false);
+                    ai1 = new Player(char_cards.get(1).name, char_cards.get(1).hp, role_cards.get(1).role, true);
+                    ai2 = new Player(char_cards.get(2).name, char_cards.get(2).hp, role_cards.get(2).role, true);
+                    ai3 = new Player(char_cards.get(3).name, char_cards.get(3).hp, role_cards.get(3).role, true);
+                    break;
+                case 4:
+                    role_cards.add(new Role_Cards("Sheriff"));
+                    role_cards.add(new Role_Cards("Renegade"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Deputy"));
+                    Collections.shuffle(role_cards);
+                    human = new Player(char_cards.get(0).name, char_cards.get(0).hp, role_cards.get(0).role, false);
+                    ai1 = new Player(char_cards.get(1).name, char_cards.get(1).hp, role_cards.get(1).role, true);
+                    ai2 = new Player(char_cards.get(2).name, char_cards.get(2).hp, role_cards.get(2).role, true);
+                    ai3 = new Player(char_cards.get(3).name, char_cards.get(3).hp, role_cards.get(3).role, true);
+                    ai4 = new Player(char_cards.get(4).name, char_cards.get(4).hp, role_cards.get(4).role, true);
+                    break;
+                case 5:
+                    role_cards.add(new Role_Cards("Sheriff"));
+                    role_cards.add(new Role_Cards("Renegade"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Deputy"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    Collections.shuffle(role_cards);
+                    human = new Player(char_cards.get(0).name, char_cards.get(0).hp, role_cards.get(0).role, false);
+                    ai1 = new Player(char_cards.get(1).name, char_cards.get(1).hp, role_cards.get(1).role, true);
+                    ai2 = new Player(char_cards.get(2).name, char_cards.get(2).hp, role_cards.get(2).role, true);
+                    ai3 = new Player(char_cards.get(3).name, char_cards.get(3).hp, role_cards.get(3).role, true);
+                    ai4 = new Player(char_cards.get(4).name, char_cards.get(4).hp, role_cards.get(4).role, true);
+                    ai5 = new Player(char_cards.get(5).name, char_cards.get(5).hp, role_cards.get(5).role, true);
+                    break;
+                case 6:
+                    role_cards.add(new Role_Cards("Sheriff"));
+                    role_cards.add(new Role_Cards("Renegade"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Deputy"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Deputy"));
+                    Collections.shuffle(role_cards);
+                    human = new Player(char_cards.get(0).name, char_cards.get(0).hp, role_cards.get(0).role, false);
+                    ai1 = new Player(char_cards.get(1).name, char_cards.get(1).hp, role_cards.get(1).role, true);
+                    ai2 = new Player(char_cards.get(2).name, char_cards.get(2).hp, role_cards.get(2).role, true);
+                    ai3 = new Player(char_cards.get(3).name, char_cards.get(3).hp, role_cards.get(3).role, true);
+                    ai4 = new Player(char_cards.get(4).name, char_cards.get(4).hp, role_cards.get(4).role, true);
+                    ai5 = new Player(char_cards.get(5).name, char_cards.get(5).hp, role_cards.get(5).role, true);
+                    ai6 = new Player(char_cards.get(6).name, char_cards.get(6).hp, role_cards.get(6).role, true);
+                    break;
+                case 7:
+                    role_cards.add(new Role_Cards("Sheriff"));
+                    role_cards.add(new Role_Cards("Renegade"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Deputy"));
+                    role_cards.add(new Role_Cards("Outlaw"));
+                    role_cards.add(new Role_Cards("Deputy"));
+                    role_cards.add(new Role_Cards("Renegade"));
+                    Collections.shuffle(role_cards);
+                    human = new Player(char_cards.get(0).name, char_cards.get(0).hp, role_cards.get(0).role, false);
+                    ai1 = new Player(char_cards.get(1).name, char_cards.get(1).hp, role_cards.get(1).role, true);
+                    ai2 = new Player(char_cards.get(2).name, char_cards.get(2).hp, role_cards.get(2).role, true);
+                    ai3 = new Player(char_cards.get(3).name, char_cards.get(3).hp, role_cards.get(3).role, true);
+                    ai4 = new Player(char_cards.get(4).name, char_cards.get(4).hp, role_cards.get(4).role, true);
+                    ai5 = new Player(char_cards.get(5).name, char_cards.get(5).hp, role_cards.get(5).role, true);
+                    ai6 = new Player(char_cards.get(6).name, char_cards.get(6).hp, role_cards.get(6).role, true);
+                    ai7 = new Player(char_cards.get(7).name, char_cards.get(7).hp, role_cards.get(7).role, true);
+                    break;
+            }
+        }while(players < 2 || players > 7);
+        System.out.println("_________________________________________________________");
+        System.out.println("Human is " + human.name + ", has " + human.health + ", and is a " + human.role);
+        System.out.println("AI1 is " + ai1.name + ", has " + ai1.health + ", and is a " + ai1.role);
+        System.out.println("AI2 is " + ai2.name + ", has " + ai2.health + ", and is a " + ai2.role);
+        System.out.println("AI3 is " + ai3.name + ", has " + ai3.health + ", and is a " + ai3.role);
+        System.out.println("AI4 is " + ai4.name + ", has " + ai4.health + ", and is a " + ai4.role);
+        System.out.println("AI5 is " + ai5.name + ", has " + ai5.health + ", and is a " + ai5.role);
+        System.out.println("AI6 is " + ai6.name + ", has " + ai6.health + ", and is a " + ai6.role);
+        System.out.println("AI7 is " + ai7.name + ", has " + ai7.health + ", and is a " + ai7.role);
+        
+        
     }
     
 }
