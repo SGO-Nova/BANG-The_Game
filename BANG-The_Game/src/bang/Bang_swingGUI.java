9947bf34f1b0c174aaec900d6b6bdc6d4315e42f
Shafat Rakin <shafz047@users.noreply.github.com>
2020-03-27 00:18:23 -0500

    GUI using swing

--- BANG-The_Game/src/bang/Bang_swingGUI.java
+++ BANG-The_Game/src/bang/Bang_swingGUI.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Shafat Rakin
+ * CS 2365-001
+ * This file implements a GUI using java.swing
  */
 
 package bang;
