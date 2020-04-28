b3a635d5b148514fa9ead2fb3f578213f2900522
sagarikakc <62684429+sagarikakc@users.noreply.github.com>
2020-04-28 01:20:29 -0500

    Add files via upload

---
+++ BANG-The_Game/src/bang/Picture.java
@@ -0,0 +1,122 @@
+//This is just example of how I am going to upload images for each character's names 
+//@Ryan I know this is incomplete and showing bunch of errors so please don't delete it
+//Let me know if i need to make any changes or you want me to do in different way
+
+/*
+*Sagarika KC
+*Collaboration with Shafat Rakin
+*CS 2365-001
+*Purpose: To assign pictures for each characters 
+*/
+
+package bang;
+
+import static bang.BANG.deathSeq;
+import java.util.*;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Picture {
+    
+   //Uploading images for all the characters 
+    
+    Image img_bartCassidy = new Image(new FileInputStream("source")); 
+    Image img_blackJack = new Image(new FileInputStream("source"));
+    Image img_calamityJanet = new Image(new FileInputStream("source")); 
+    Image img_elGringo = new Image(new FileInputStream("source")); 
+    Image img_jesseJones = new Image(new FileInputStream("source")); 
+    Image img_joursonnais = new Image(new FileInputStream("source"));
+    Image img_kitCarlson = new Image(new FileInputStream("source")); 
+    Image img_luckyDuke = new Image(new FileInputStream("source")); 
+    Image img_paulRegret = new Image(new FileInputStream("source")); 
+    Image img_pedroRamirez = new Image(new FileInputStream("source")); 
+    Image img_blackJack = new Image(new FileInputStream("source"));
+    Image img_pedroRamirez = new Image(new FileInputStream("source")); 
+    Image img_roseDoolan = new Image(new FileInputStream("source")); 
+    Image img_sidKetchum = new Image(new FileInputStream("source")); 
+    Image img_slabTheKiller = new Image(new FileInputStream("source")); 
+    Image img_suzyLafayette = new Image(new FileInputStream("source"));
+    Image img_vultureSam = new Image(new FileInputStream("source")); 
+    Image img_willyTheKid = new Image(new FileInputStream("source")); 
+    Image img_belleStart = new Image(new FileInputStream("source")); 
+    Image img_gregDigger= new Image(new FileInputStream("source"));   
+   
+   //Assigning each character's names with their images 
+    
+                        
+    if(Character_Cards.name.contains ( "Bart Cassidy")){
+        ImageView bartCassidy = new ImageView(img_bartCassidy);     
+                        }
+    
+    if(Character_Cards.name.contains ( "Black Jack")){
+        ImageView table = new ImageView(img_blackJack);                
+                        }
+    
+    if(Character_Cards.name.contains ( "Calamity Janet")){
+        ImageView table = new ImageView(img_calamityJanet);      
+                        }
+    
+    if(Character_Cards.name.contains ( "El Gringo")){
+        ImageView bartCassidy = new ImageView(img_elGringo);     
+                        }
+  
+    if(Character_Cards.name.contains ( "Jesse Jones")){
+        ImageView table = new ImageView(img_jesseJones);                 
+                        }
+    
+    if(Character_Cards.name.contains ( "Joursonnais")){
+        ImageView table = new ImageView(img_joursonnais);                
+                        }
+    
+     if(Character_Cards.name.contains ( "Kit Carlson")){
+        ImageView table = new ImageView(img_kitCarlson);                
+                        }
+    
+    if(Character_Cards.name.contains ( "Lucky Duke")){
+        ImageView table = new ImageView(img_luckyDuke);      
+                        }
+    
+    if(Character_Cards.name.contains ( "Paul Regret")){
+        ImageView bartCassidy = new ImageView(img_paulRegret);     
+                        }
+      
+
+    if(Character_Cards.name.contains ( "Pedro Ramirez")){
+        ImageView table = new ImageView(img_pedroRamirez);                 
+  
+    
+    if(Character_Cards.name.contains ( "Rose Doolan")){
+        ImageView table = new ImageView(img_roseDoolan);                
+                        }
+    
+    if(Character_Cards.name.contains ( "Sid Ketchum")){
+        ImageView table = new ImageView(img_sidKetchum);      
+                        }
+     if(Character_Cards.name.contains ( "Slab The Killer")){
+        ImageView table = new ImageView(img_slabTheKiller);                
+                        }
+    
+    if(Character_Cards.name.contains ( "Suzy Lafayette")){
+        ImageView table = new ImageView(img_suzyLafayette);      
+                        }
+    
+    if(Character_Cards.name.contains ( "Vulture Sam")){
+        ImageView bartCassidy = new ImageView(img_vultureSam);     
+                        }
+      
+
+    if(Character_Cards.name.contains ( "Willy The Kid")){
+        ImageView table = new ImageView(img_willyTheKid);                 
+                        }
+    
+    if(Character_Cards.name.contains ( "Belle Star")){
+        ImageView table = new ImageView(img_belleStar);                
+                        }
+    
+    if(Character_Cards.name.contains ( "Greg Digger")){
+        ImageView table = new ImageView(img_gregDigger);      
+                        }
+    
